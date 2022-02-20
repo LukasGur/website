3d2b3f2b5224c0d8510c29a5bac52df04eff288b
Lukáš Gurecký <l.gurecky@seznam.cz>
2022-02-20 12:24:49 +0100

    feat: add google analytics

---
+++ src/components/GoogleAnalytics.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	$: {
+		/* @ts-ignore */
+		if (typeof gtag !== 'undefined') {
+			console.log($page.url)
+			/* @ts-ignore */
+			gtag('config', 'G-YMPL50YVZ7', {
+				page_path: $page.url
+			})
+		}
+	}
+</script>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,10 @@
 	import '../app.css'
 	import Header from '$/layout/Header.svelte'
 	import Footer from '$/layout/Footer.svelte'
+	import GoogleAnalytics from '$/components/GoogleAnalytics.svelte'
 </script>
 
+<GoogleAnalytics />
 <Header />
 <div class="container mt-20">
 	<slot />
